5cc25075ef9c3a9a651f0aa8780a0a5b999b5ae0
Francisco Eduardo <franciscoeduardo@MacBook-Air-de-Francisco.local>
2025-04-23 18:29:50 -0300

    created upload-image, implemented documentation with swagger and configured vitest

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,10 +1,52 @@
-import fastify from "fastify";
-import {fastifyCors} from "@fastify/cors";
+import { fastifyCors } from '@fastify/cors'
+import fastifyMultipart from '@fastify/multipart'
+import fastifySwagger from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import fastify from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { uploadImageRoute } from './routes/upload-image'
 
 const server = fastify()
 
-server.register(fastifyCors, { origin: '*'})
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
 
-server.listen({port: 3333, host: '0.0.0.0' }).then(() => {
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(401).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+
+  // Envia o erro para alguma ferramenta de observabilidade (Sentry/Datadog/Grafana/OTel)
+  console.error(error)
+
+  return reply.status(500).send({ message: 'Internal server error' })
+})
+
+server.register(fastifyCors, { origin: '*' })
+
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Upload Server',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+server.register(fastifySwaggerUi, { routePrefix: 'docs' })
+
+server.register(uploadImageRoute)
+
+server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP Server Running...')
 })
